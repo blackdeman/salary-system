8b98274087575d33d1d8b11d942c40ea436979f7
Andrew Demchenko <blackdeman@gmail.com>
2014-12-08 23:16:17 +0300

    Go go JPA

--- src/main/java/by/dreamteam/database/MainDAO.java
+++ src/main/java/by/dreamteam/database/MainDAO.java
@@ -1,14 +1,18 @@
 package by.dreamteam.database;
 
+import javax.annotation.Resource;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.sql.DataSource;
+
 /**
  * @author Andrew
  * @version 1.0
  * @created 06-Dec-2014 3:35:02 PM
  */
 public class MainDAO {
-
-    public MainDAO() {
-
-    }
+    
+    @PersistenceContext
+    protected EntityManager em;
 
 }//end MainDAO
