a3438592975ce23e4d2d312d2f88c02dad7d9c03
Dmitry Kaganovich <kaganovichd@gmail.com>
2014-12-08 23:45:55 +0200

    Login with realm possibility added.

---
+++ files/salary-system-fill-account.sql
@@ -0,0 +1,6 @@
+USE `salary_system` ;
+
+INSERT INTO users VALUES('admin','admin');
+INSERT INTO users VALUES('dimon','moget');
+INSERT INTO groups VALUES('admin','admin');
+INSERT INTO groups VALUES('dimon','user');
