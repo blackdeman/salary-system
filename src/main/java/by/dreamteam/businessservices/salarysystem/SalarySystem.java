d6038a664fc1ae79126a7ef73be4b463ebecfd43
Dmitry Kaganovich <kaganovichd@gmail.com>
2014-12-13 13:24:19 +0200

    Logger fixed.

--- src/main/java/by/dreamteam/businessservices/salarysystem/SalarySystem.java
+++ src/main/java/by/dreamteam/businessservices/salarysystem/SalarySystem.java
@@ -1,14 +1,38 @@
 package by.dreamteam.businessservices.salarysystem;
 
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author Andrew
  * @version 1.0
  * @created 06-Dec-2014 3:35:03 PM
  */
-public class SalarySystem {
+@SessionScoped
+@Named
+public class SalarySystem implements Serializable {
 
-	public SalarySystem(){
+    private static final Logger LOG = LoggerFactory.getLogger(SalarySystem.class);
 
-	}
+    public SalarySystem() {        
+    }
+    
+    public String getMsg() {
+        LOG.info("!!!!!!!!!!!!!!!!!LOGGGGGG!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        LOG.info("!!!!!!!!!!!!!!!!!LOGGGGGG!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        LOG.info("!!!!!!!!!!!!!!!!!LOGGGGGG!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        LOG.info("!!!!!!!!!!!!!!!!!LOGGGGGG!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        LOG.info("!!!!!!!!!!!!!!!!!LOGGGGGG!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        LOG.info("!!!!!!!!!!!!!!!!!LOGGGGGG!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        LOG.info("!!!!!!!!!!!!!!!!!LOGGGGGG!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        LOG.info("!!!!!!!!!!!!!!!!!LOGGGGGG!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        LOG.info("!!!!!!!!!!!!!!!!!LOGGGGGG!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        LOG.info("!!!!!!!!!!!!!!!!!LOGGGGGG!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        LOG.info("!!!!!!!!!!!!!!!!!LOGGGGGG!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        return "Welcome to log!";
+    }
 
 }//end SalarySystem
